a3d773f20c35c9df340bcd9ed514dca73f63075e
phoenixcrackers <phoenixcrackersfwc@gmail.com>
2025-11-30 14:23:15 +0530

    ho

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
